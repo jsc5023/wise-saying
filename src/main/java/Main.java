b9a3e53cdecaccd523fd2fe4dddd108c09a4532d
SeongCheol Jung <kir5121@naver.com>
2024-12-23 16:04:19 +0900

    [feat] - 변수형 으로 선언하도록 변경

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,3 @@
-package Main;
-
 import java.util.Scanner;
 
 public class Main {
@@ -14,6 +12,7 @@ class App {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("== 명언 앱 ==");
+        int lastNo = 1;
 
         while(true) {
             System.out.print("명령) ");
@@ -23,13 +22,15 @@ class App {
                 System.out.println("명언 앱을 종료합니다.");
                 break;
             } else if(command.equals("등록")) {
+
                 System.out.print("명언 : ");
                 scanner.nextLine(); // 입력값 가져옴. 입력값이 없으면 기다린다. 엔터를 쳐야 입력이 완료됨. 그래야 넘어감
 
                 System.out.print("작가 : ");
                 scanner.nextLine();
 
-                System.out.println("1번 명언이 등록되었습니다.");
+                System.out.println(lastNo + "번 명언이 등록되었습니다.");
+                lastNo++;
             }
         }
     }

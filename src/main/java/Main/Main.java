ae6c1f80d1998676333174e6d9f7207608c8c3d5
SeongCheol Jung <kir5121@naver.com>
2024-12-23 15:47:27 +0900

    명령문 추가

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,7 +1,36 @@
 package Main;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        App app = new App();
+        app.run();
+    }
+}
 
+class App {
+    public void run() {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("== 명언 앱 ==");
+
+        while(true) {
+            System.out.print("명령) ");
+            String command = scanner.nextLine();
+
+            if(command.equals("종료")) {
+                System.out.println("명언 앱을 종료합니다.");
+                break;
+            } else if(command.equals("등록")) {
+                System.out.print("명언 : ");
+                scanner.nextLine(); // 입력값 가져옴. 입력값이 없으면 기다린다. 엔터를 쳐야 입력이 완료됨. 그래야 넘어감
+
+                System.out.print("작가 : ");
+                scanner.nextLine();
+
+                System.out.println("1번 명언이 등록되었습니다.");
+            }
+        }
     }
 }

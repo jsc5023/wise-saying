7dcd283d22716eb9f9227569d3b38eb3ed4b54d6
SeongCheol Jung <kir5121@naver.com>
2024-12-24 12:12:46 +0900

    [feat] - add 기능 추가 및 class의 보안을 위한 get기능 추가

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.time.LocalDateTime;
 import java.util.Scanner;
 
 public class Main {
@@ -8,12 +9,18 @@ public class Main {
 }
 
 class App {
+
+    // 클래스에서 함수들이 공유해야 하는 변수 -> 인스턴스 변수
+    private int lastId = 0;
+    private int wiseSayingSize = 0;
+    private WiseSaying[] wiseSayingList = new WiseSaying[3];
+
     public void run() {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("== 명언 앱 ==");
-        int lastNo = 0;
+        add("꿈을 지녀라. 그러면 어려운 현실을 이길 수 있다.", "월트 디즈니");
 
+        System.out.println("== 명언 앱 ==");
         while(true) {
             System.out.print("명령) ");
             String command = scanner.nextLine();
@@ -21,16 +28,59 @@ class App {
             if(command.equals("종료")) {
                 System.out.println("명언 앱을 종료합니다.");
                 break;
+
             } else if(command.equals("등록")) {
 
                 System.out.print("명언 : ");
-                scanner.nextLine(); // 입력값 가져옴. 입력값이 없으면 기다린다. 엔터를 쳐야 입력이 완료됨. 그래야 넘어감
+                String content = scanner.nextLine();
 
                 System.out.print("작가 : ");
-                scanner.nextLine();
+                String author = scanner.nextLine();
 
-                System.out.println("%d번 명언이 등록되었습니다.".formatted(++lastNo));
+                add(content, author); // 1. 함수로 분리 -> 코드가 줄어든다. 가독성이 올라간다. 2. 재활용성(중복 제거)
+
+                System.out.println("%d번 명언이 등록되었습니다.".formatted(lastId));
+            } else if(command.equals("목록")) {
+
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("----------------------");
+
+                for(int i = 0; i < wiseSayingSize; i++) {
+                    WiseSaying wiseSaying = wiseSayingList[i];
+                    System.out.println("%d / %s / %s".formatted(wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getContent()));
+                }
             }
         }
     }
+
+    public void add(String content, String author) {
+        WiseSaying wiseSaying = new WiseSaying(++lastId, content, author);
+        wiseSayingList[wiseSayingSize++] = wiseSaying;
+    }
+
+}
+
+class WiseSaying {
+    private int id;
+    private String content;
+    private String author;
+
+    WiseSaying(int id, String content, String author) {
+        this.id = id;
+        this.content = content;
+        this.author = author;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
 }

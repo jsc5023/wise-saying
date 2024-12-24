a830cb5f945756de7a9118055bf0966f0154107c
SeongCheol Jung <kir5121@naver.com>
2024-12-24 15:00:52 +0900

    [feat] - 클래스별 기능을 따로 분리

---
+++ src/main/java/App.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class App {
+    private int lastId = 0;
+    private final ArrayList<WiseSaying> wiseSayingList = new ArrayList<>();
+
+    Scanner scanner = new Scanner(System.in);
+
+    public void run() {
+
+        add("꿈을 지녀라. 그러면 어려운 현실을 이길 수 있다.", "월트 디즈니");
+        add("현재를 사랑하라", "작자 미상");
+
+        System.out.println("== 명언 앱 ==");
+        while (true) {
+            System.out.print("명령) ");
+            String command = scanner.nextLine();
+
+            if (command.equals("종료")) {
+                System.out.println("명언 앱을 종료합니다.");
+                break;
+
+            } else if (command.equals("등록")) {
+                writeWiseSaying();
+            } else if (command.equals("목록")) {
+                printWiseSayingList();
+            } else if(command.startsWith("삭제?id=")) {
+                deleteWiseSaying();
+            }
+        }
+    }
+
+    private void deleteWiseSaying() {
+        System.out.println("삭제");
+    }
+
+    private void printWiseSayingList() {
+        System.out.println("번호 / 작가 / 명언");
+        System.out.println("----------------------");
+
+        for(WiseSaying wiseSaying : wiseSayingList.reversed()) {
+            System.out.println("%d / %s / %s".formatted(wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getContent()));
+        }
+    }
+
+    private void writeWiseSaying() {
+        System.out.print("명언 : ");
+        String content = scanner.nextLine();
+
+        System.out.print("작가 : ");
+        String author = scanner.nextLine();
+
+        add(content, author);
+        System.out.println("%d번 명언이 등록되었습니다.".formatted(lastId));
+    }
+
+    public void add(String content, String author) {
+        WiseSaying wiseSaying = new WiseSaying(++lastId, content, author);
+        wiseSayingList.add(wiseSaying);
+    }
+
+}
